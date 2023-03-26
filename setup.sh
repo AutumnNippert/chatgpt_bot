667c13b8cd9c330112da6bb77f7f5d8598c1db1d
Chris <chrisnippert22@gmail.com>
2023-03-26 01:50:08 -0400

    finished setup.sh

--- setup.sh
+++ setup.sh
@@ -13,3 +13,27 @@ activate
 
 python3 -m pip install -r requirements.txt
 python3 setup_nltk.py
+
+# check for .env file
+if [ ! -f .env ]; then
+    echo "Creating .env file"
+    cp .env.example .env
+fi
+
+# check for if all 3 variables are set in the .env file
+# OPENAI_API_KEY=stuff
+# DISCORD_TOKEN=stuff
+# WATSON_API_KEY=stuff
+# Read the .env file and save the lines containing our variables to a variable
+vars=$(grep -e "^OPENAI_API_KEY=" -e "^DISCORD_TOKEN=" -e "^WATSON_API_KEY=" .env)
+
+# Check if all three variables are present
+if [ $(echo "$vars" | wc -l) -eq 3 ]; then
+  echo "All three variables are set!"
+else
+  echo "Not all three variables are set."
+  echo "Please set the following variables in the .env file:"
+  echo "OPENAI_API_KEY"
+  echo "DISCORD_TOKEN"
+  echo "WATSON_API_KEY"
+fi
